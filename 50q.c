13cbbd24ac0b62e79bddef0c460b69375e901f3c
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2021-03-16 09:54:33 +0000

    Update 50q.c

--- 50q.c
+++ 50q.c
@@ -101,3 +101,12 @@ int qDig(int n)
     return r;
 }
 
+//7: Concatena 2 strings
+char *mystrcat(char s1[], char s2[]) {
+    int i,j;
+    for (i = 0; s1[i] != '\0'; i++);
+    for (j = 0; s2[j] != '\0'; j++)
+        s1[i+j] = s2[j];
+    s1[i+j] = '\0';
+    return s1;
+}
