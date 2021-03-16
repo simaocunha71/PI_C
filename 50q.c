458a0b2d10b2fbe01b25935b3c6943bdd8f09096
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2021-03-16 09:58:15 +0000

    Update 50q.c

--- 50q.c
+++ 50q.c
@@ -110,3 +110,13 @@ char *mystrcat(char s1[], char s2[]) {
     s1[i+j] = '\0';
     return s1;
 }
+
+//8: Copia 2 strings
+char *mystrcpy(char s1[], const char s2[]) {
+    int i;
+    for (i=0; s2[i] != '\0'; i++)
+        s1[i] = s2[i];
+    s1[i] = '\0';
+    return s1;
+}
+
