e59258106744373f45108cdf119b6a9fe966141c
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2021-05-10 10:18:23 +0100

    Update ficha9.c

--- ficha9/ficha9.c
+++ ficha9/ficha9.c
@@ -49,3 +49,55 @@ ABin maisEsquerda (ABin a){
 }
 
 //alinea (d) -  escreve no ecran os elementos da arvore que estao ao nivel l (Considere que a raiz de uma arvore se encontra no nivel 0) 
+void imprimeNivel (ABin a, int l){
+    if (a == NULL)
+        printf("NULL ");
+    else if (l == 1)
+        printf("%d ",a->valor);
+    else{
+        imprimeNivel (a->esq,l-1);
+        imprimeNivel (a->dir,l-1);
+    }
+}
+
+//alinea (e) - testa se x ocorre na arvore. Nao assuma qualquer propriedade sobre a arvore a.
+int procuraE (ABin a, int x){
+    int r = 0;
+    if (a == NULL || a->valor == x)
+        r = 1;
+    else
+        return procuraE(a->esq,x) || procuraE(a->dir,x);
+}
+
+//Exercicio 2
+//alinea(a) - testa se x ocorre na arvore binaria de procura, devolvendo o nó onde ocorre (caso não seja possível, devolve NULL)
+struct nodo *procura (ABin a, int x){
+    if (a == NULL || a->valor == x)
+        return a;
+    else if (x > a->valor)
+        return procura(a->dir,x);
+    else
+        return procura(a->esq,x);
+}
+
+//alinea(b) - calcula o nivel a que o elemento ocorre na arvore (-1 caso nao exista) - CORRIGIR
+int nivel (ABin a, int x){
+    if (x == a->valor)
+        return 0;
+    else if (x > a->valor)
+        return 1 + nivel(a->esq,x);
+    else
+        return 1 + nivel(a->dir,x);
+    return -1;
+}
+
+//alinea(c) - e imprime no ecran, por ordem crescente, todos os elementos da arvore que sao (estritamente) menores do que um dado valor - CORRIGIR
+void imprimeAte (ABin a, int x){
+    if (a == NULL)
+        return;
+    if (a->valor < x){
+        printf("%d ",a->valor);
+        imprimeAte (a->esq,x);
+        imprimeAte (a->dir,x);
+    }
+}
